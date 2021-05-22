df43cc66b88766210a72533ba1311fcaa1aaa721
inji <iij0573@naver.com>
2021-05-22 16:57:34 +0900

    Initial commit

---
+++ src/main/java/com/example/bookprojec/BookProjecApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookprojec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookProjecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookProjecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookprojec/BookProjecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookprojec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookProjecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
